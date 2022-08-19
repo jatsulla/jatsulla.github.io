92c76ead8f0a558e1f34678e8728dd3f210df8a9
Javier <javienllamas@gmail.com>
2022-08-18 23:41:11 -0500

    Adición del diseño para pantallas y mejoras del código js

--- script.js
+++ script.js
@@ -11,9 +11,14 @@ function encriptarTexto() {
 
     var texto = txtOriginal.value;
 
-    ocultarImagen();
-    txtFinal.value = texto.replace(/e/gi,"enter").replace(/i/gi,"imes").replace(/a/gi,"ai").replace(/o/gi,"ober").replace(/u/gi,"ufat");
-    txtOriginal.value = "";
+    if (texto == "") {
+        alert("No se detectó ningún mensaje para encriptar o desencriptar, por favor inserte su texto e inténtelo nuevamente");
+    } else {
+        ocultarImagen();
+        txtFinal.value = texto.replace(/e/gi,"enter").replace(/i/gi,"imes").replace(/a/gi,"ai").replace(/o/gi,"ober").replace(/u/gi,"ufat");
+        txtOriginal.value = "";
+    }
+
 }
 
 function desencriptarTexto() {
@@ -22,9 +27,14 @@ function desencriptarTexto() {
 
     var texto = txtOriginal.value;
 
-    ocultarImagen();
-    txtFinal.value = texto.replace(/enter/gi,"e").replace(/imes/gi,"i").replace(/ai/gi,"a").replace(/ober/gi,"o").replace(/ufat/gi,"u");
-    txtOriginal.value = "";
+    if (texto == "") {
+        alert("No se detectó ningún mensaje para encriptar o desencriptar, por favor inserte su texto e inténtelo nuevamente");
+    } else {
+        ocultarImagen();
+        txtFinal.value = texto.replace(/enter/gi,"e").replace(/imes/gi,"i").replace(/ai/gi,"a").replace(/ober/gi,"o").replace(/ufat/gi,"u");
+        txtOriginal.value = "";
+    }
+
 }
 
 function copy() {
